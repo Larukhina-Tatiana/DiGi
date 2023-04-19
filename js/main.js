ceea1bed1d2e3d479795c34da4b2a04e166a7e06
Татьяна <ltasya72@gmail.com>
2023-04-20 00:28:24 +0300

    докачаны шрифты, готово hero

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(function(){
-
+$(function () {
+  $(".hero__slider").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="/images/icons/next-arrow.svg" alt="next arrow"></button>',
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="/images/icons/prev-arrow.svg" alt="prev arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {},
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
 });
