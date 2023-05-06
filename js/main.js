944baebbfda485c0458943a3295990665cb98c6d
Татьяна <ltasya72@gmail.com>
2023-05-06 11:55:08 +0300

    Закончена работа, нет слайдера на цитаты

--- js/main.js
+++ js/main.js
@@ -19,3 +19,27 @@ $(function () {
     ],
   });
 });
+
+// $(function () {
+//   $(".testimonial__slider").slick({
+//     dots: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="./images/icons/next-arrow.svg" alt="next arrow"></button>',
+//     prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="./images/icons/prev-arrow.svg" alt="prev arrow"></button>',
+//     responsive: [
+//       {
+//         breakpoint: 1024,
+//         settings: {},
+//       },
+//       {
+//         breakpoint: 375,
+//         settings: {
+//           nextArrow: false,
+//           arrows: false,
+//           prevArrow: false,
+//         },
+//       },
+//     ],
+//   });
+// });
