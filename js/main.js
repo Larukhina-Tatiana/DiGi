b1f99ea0d71778be9edb4822681842eb4e98db86
Татьяна <ltasya72@gmail.com>
2023-05-09 00:25:40 +0300

    сделан слайдер цитат

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ $(function () {
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="./images/icons/next-arrow.svg" alt="next arrow"></button>',
-    prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="./images/icons/prev-arrow.svg" alt="prev arrow"></button>',
+    nextArrow:
+      '<button type="button" class="slick-arrow slick-next"><img src="./images/icons/next-arrow.svg" alt="next arrow"></button>',
+    prevArrow:
+      '<button type="button" class="slick-arrow slick-prev"><img src="./images/icons/prev-arrow.svg" alt="prev arrow"></button>',
     responsive: [
       {
         breakpoint: 1024,
@@ -20,26 +22,18 @@ $(function () {
   });
 });
 
-// $(function () {
-//   $(".testimonial__slider").slick({
-//     dots: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="./images/icons/next-arrow.svg" alt="next arrow"></button>',
-//     prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="./images/icons/prev-arrow.svg" alt="prev arrow"></button>',
-//     responsive: [
-//       {
-//         breakpoint: 1024,
-//         settings: {},
-//       },
-//       {
-//         breakpoint: 375,
-//         settings: {
-//           nextArrow: false,
-//           arrows: false,
-//           prevArrow: false,
-//         },
-//       },
-//     ],
-//   });
+$(function () {
+  $(".testimonial__slider").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  });
+});
+
+// var swiper = new Swiper(".mySwiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//     dynamicBullets: true,
+//   },
 // });
